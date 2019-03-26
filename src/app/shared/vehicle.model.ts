d6739ed7c2b59533d8826abf2c6acd35c14dd646
Stephen Paine <smpaine@gmail.com>
2019-03-26 17:18:21 -0400

    Updated edit vehicle form

--- src/app/shared/vehicle.model.ts
+++ src/app/shared/vehicle.model.ts
@@ -1,23 +1,19 @@
 export class Vehicle{
     id: number;
     name: string;
-    description: string;
+    make: string;
+    model: string;
+    year: string;
+    purchased: Date;
+
     editing: boolean = false;
-    imageUrl: string;
-    contractTypeId: number;
-    isActive: boolean;
-    contractSignedOn: Date;
-    budget: number;
-    originalVehicle: Vehicle;
 
     constructor(obj?: any){
         this.id = obj && obj.id || null;
         this.name = obj && obj.name || null;
-        this.description = obj && obj.description || null;
-        this.contractTypeId = obj && obj.contractTypeId || null;
-        this.isActive = obj && obj.isActive || false;
-        this.contractSignedOn = obj && obj.contractSignedOn || new Date();
-        this.budget = obj && obj.budget || 0;
-        this.imageUrl = "http://placehold.it/500x300/e4d2ba";
+        this.make = obj && obj.make || null;
+        this.model = obj && obj.model || null;
+        this.year = obj && obj.year || false;
+        this.purchased = obj && obj.purchased || new Date();
     }
 }
