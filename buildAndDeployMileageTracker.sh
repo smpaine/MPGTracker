f8ea9ac0191e583e6d4a8dd13650af6ea339f55a
Stephen Paine <smpaine@gmail.com>
2019-03-30 13:27:23 -0400

    Updated build script

--- buildAndDeployMileageTracker.sh
+++ buildAndDeployMileageTracker.sh
@@ -1,1 +1,1 @@
-ng build --output-path /usr/local/var/www/vhosts/nameniap.com/spaine/MPGTracker_test/ --base-href=/spaine/MPGTracker_test/
+ng build --configuration=production --output-path /usr/local/var/www/vhosts/nameniap.com/spaine/MPGTracker/
