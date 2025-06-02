e628fa16b1265a47179606ddbf279b2f38b7218a
Stephen Paine <smpaine@gmail.com>
2025-06-02 16:16:03 -0400

    Updated buildAndDeploy script

--- buildAndDeployMileageTracker.sh
+++ buildAndDeployMileageTracker.sh
@@ -1,2 +1,6 @@
 export NODE_OPTIONS=--openssl-legacy-provider
 ng build --configuration=production --base-href "/spaine/MPGTracker/" --output-path /usr/local/var/www/vhosts/nameniap.com/spaine/MPGTracker/
+
+cd /usr/local/var/www/vhosts/nameniap.com/spaine/MPGTracker/
+chgrp www .htaccess favicon.ico apple-touch-icon.png
+
